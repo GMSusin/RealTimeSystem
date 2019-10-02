97eaba534e67b4669b1ff683986a72184be94987
Susin <40000680+GMSusin@users.noreply.github.com>
2019-10-02 18:52:02 -0300

    only printing output

--- STR_LST.c
+++ STR_LST.c
@@ -26,23 +26,23 @@ int main(void)
 	int n_tasks = 0, time = 0, i, j, t;
 	int leitura = 0;
 	int iddle = 0;
+	int iddle_total =0;
 	char Gantt[MAX_T];
 	int Sobra[MAX_T];
 	int flag = 1, clear;
 	int aux_ponteiro = 0, aux_slack = 65535, aux_prioridade = 65535;
-	int iddle_total = 0;
 	int flag_iddle = 1;
 	while (1)
 	{
 		//read
-		printf("Nº de Tarefas \nTempo de Simulacao\n");
+		//printf("Nº de Tarefas \nTempo de Simulacao\n");
 		scanf("%d %d", &n_tasks, &time);
 		if ((n_tasks == 0) || (time == 0))
 			break;
 		for (leitura = 0; leitura < n_tasks; leitura++)
 		{
 			task[leitura].id = 'A' + leitura; 
-			printf("\nTempo de Computacao (C) \nPeriodo (P) \nDeadline(D)\n");
+			//printf("\nTempo de Computacao (C) \nPeriodo (P) \nDeadline(D)\n");
 			scanf("%d %d %d", &(task[leitura].c), &(task[leitura].p), &(task[leitura].d));
 			task[leitura].Sobra_ctr = 1;
 			Sobra[leitura] = 1;
@@ -73,43 +73,43 @@ int main(void)
 
 			for (i = 0; i < n_tasks; i++)
 			{
-				printf("Tempo Atual %d\nPeriodo da Task %c==%d \n", t, task[i].id, task[i].p);
-				printf("Calculo Periodo da Task %c %d==%d \n", task[i].id, (task[i].p_aux), (t));
+				//printf("Tempo Atual %d\nPeriodo da Task %c==%d \n", t, task[i].id, task[i].p);
+				//printf("Calculo Periodo da Task %c %d==%d \n", task[i].id, (task[i].p_aux), (t));
 				if (t == 0)
 				{
-					printf("Zerei A Vida \n");
+					//printf("Zerei A Vida \n");
 					task[i].p_aux = task[i].p;
 					task[i].d_aux = task[i].d;
 					task[i].Comp_Sobra = task[i].c;
-					printf("Tarefa %c\nComp_Sobra==%d\nComp da Task==%d \n", task[i].id, task[i].Comp_Sobra, task[i].c);
+					//printf("Tarefa %c\nComp_Sobra==%d\nComp da Task==%d \n", task[i].id, task[i].Comp_Sobra, task[i].c);
 					task[i].slack = ((task[i].d_aux - t) - task[i].Comp_Sobra);
-					printf("Tarefa %c\nSlack==%d\n", task[i].id, task[i].slack);
+					//printf("Tarefa %c\nSlack==%d\n", task[i].id, task[i].slack);
 					flag = 1;
 					task[i].Sobra_ctr = 1;
-					printf("Zerei A Vida \n");
+					//printf("Zerei A Vida \n");
 				}
 				else if (task[i].p_aux == t)
 				{
-					printf("Zerei A Vida 2 \n");
+					//printf("Zerei A Vida 2 \n");
 					task[i].p_aux = task[i].p_aux + task[i].p;
 					task[i].d_aux = task[i].d_aux + task[i].p;
 					task[i].Comp_Sobra = task[i].c;
-					printf("Tarefa %c\nComp_Sobra==%d\nComp da Task==%d \n", task[i].id, task[i].Comp_Sobra, task[i].c);
+					//printf("Tarefa %c\nComp_Sobra==%d\nComp da Task==%d \n", task[i].id, task[i].Comp_Sobra, task[i].c);
 					task[i].slack = ((task[i].d_aux - t) - task[i].Comp_Sobra);
-					printf("Tarefa %c\nSlack==%d\n", task[i].id, task[i].slack);
+					//printf("Tarefa %c\nSlack==%d\n", task[i].id, task[i].slack);
 					flag = 1;
 					task[i].Sobra_ctr = 1;
-					printf("Zerei A Vida  2\n");
+					//printf("Zerei A Vida  2\n");
 				}
 				else
 				{
-					printf("task %c p %d \n", task[i].id, task[i].p);
-					printf("task %c sobra %d Slack %d \n", task[i].id, task[i].Comp_Sobra, task[i].slack);
+					//printf("task %c p %d \n", task[i].id, task[i].p);
+					//printf("task %c sobra %d Slack %d \n", task[i].id, task[i].Comp_Sobra, task[i].slack);
 					if ((task[i].Comp_Sobra >= 1) && (task[i].Sobra_ctr == 1))
 					{
-						printf("task %c sobra %d \n", task[i].id, task[i].Comp_Sobra);
+						//printf("task %c sobra %d \n", task[i].id, task[i].Comp_Sobra);
 						task[i].slack = ((task[i].d_aux - t) - task[i].Comp_Sobra);
-						printf("task %c slack %d \n", task[i].id, task[i].slack);
+						//printf("task %c slack %d \n", task[i].id, task[i].slack);
 						flag = 1;
 					}
 					else
@@ -117,7 +117,7 @@ int main(void)
 						task[i].Sobra_ctr = 0;
 						//Sobra[t] = task[i].Sobra_ctr;
 						task[i].slack = 65535;
-						printf("Entrei no ELSE do ELSE\n");
+						//printf("Entrei no ELSE do ELSE\n");
 					}
 				}
 			}
@@ -151,16 +151,16 @@ int main(void)
 			}
 
 			//decrementa a computacao
-			printf("Quem saiu do comparador %c\n", Tarefa.id);
+			//printf("Quem saiu do comparador %c\n", Tarefa.id);
 			for (i = 0; i < n_tasks; i++)
 			{
-				printf("Antes do IF Tarefa ID %c\nTask ID %c\nTask CompSobra %d\n", Tarefa.id, task[i].id, task[i].Comp_Sobra);
+				//printf("Antes do IF Tarefa ID %c\nTask ID %c\nTask CompSobra %d\n", Tarefa.id, task[i].id, task[i].Comp_Sobra);
 				if ((Tarefa.id == task[i].id) && (flag == 1) && (task[i].Sobra_ctr == 1))
 				{
 					task[i].Comp_Sobra = task[i].Comp_Sobra - 1;
-					printf("Depois do IF Tarefa ID %c\nTask ID %c\nTask CompSobra %d\n", Tarefa.id, task[i].id, task[i].Comp_Sobra);
+					//printf("Depois do IF Tarefa ID %c\nTask ID %c\nTask CompSobra %d\n", Tarefa.id, task[i].id, task[i].Comp_Sobra);
 					flag = 0;
-					printf("Flag==%d\nSobra Ctr %d\n", flag, task[i].Sobra_ctr);
+					//printf("Flag==%d\nSobra Ctr %d\n", flag, task[i].Sobra_ctr);
 				}
 			}
 
@@ -169,39 +169,39 @@ int main(void)
 			iddle = 0;
 			for (j = 0; j < n_tasks; j++)
 			{
-				printf("Sobra CTR %d da TAREFA %c\n", task[j].Sobra_ctr, task[j].id);
+				//printf("Sobra CTR %d da TAREFA %c\n", task[j].Sobra_ctr, task[j].id);
 				if (task[j].Sobra_ctr == 0)
 				{
-					printf("ENTREI NO IF COM TASK %c\n IDDLE == %d\n", task[j].id, iddle);
+					//printf("ENTREI NO IF COM TASK %c\n IDDLE == %d\n", task[j].id, iddle);
 					iddle++;
-					printf("SAI NO IF COM TASK %c\n IDDLE == %d\n", task[j].id, iddle);
+					//printf("SAI NO IF COM TASK %c\n IDDLE == %d\n", task[j].id, iddle);
 				}
 				if (iddle == n_tasks)
 				{
-					printf("ENTREI NO IF FINAL COM TASK %c\n IDDLE == %d\n", task[j].id, iddle);
+					//printf("ENTREI NO IF FINAL COM TASK %c\n IDDLE == %d\n", task[j].id, iddle);
 					iddle = 1;
 					Gantt[t] = '.';
-					printf("SAI NO IF FINAL COM TASK %c\n IDDLE == %d\n", task[j].id, iddle);
+					//printf("SAI NO IF FINAL COM TASK %c\n IDDLE == %d\n", task[j].id, iddle);
 
 					if ((iddle == 1) && (Gantt[t] != Gantt[t - 1]))
 					{
-
+						iddle_total = iddle_total +1;
 						Troca_Contexto = Troca_Contexto + 1;
 						Num_Premp = Num_Premp + 1;
 
 						flag_iddle = 0;
-						printf("troca de contexto==%d do iddle", Troca_Contexto);
+						//printf("troca de contexto==%d do iddle", Troca_Contexto);
 					}
 				}
 			}
 			//calcula preempcoes
 
-			printf("Task ID==%c\nSobra Ctr %d\nTask ID==%c\nSobra Ctr %d\n", Gantt[t], Sobra[t], Gantt[t - 1], Sobra[t - 1]);
+			//printf("Task ID==%c\nSobra Ctr %d\nTask ID==%c\nSobra Ctr %d\n", Gantt[t], Sobra[t], Gantt[t - 1], Sobra[t - 1]);
 
 			if ((Gantt[t] != Gantt[t - 1]) && (flag_iddle != 0))
 			{
 				Troca_Contexto++;
-				printf("troca de contexto==%d processo", Troca_Contexto);
+				//printf("troca de contexto==%d processo", Troca_Contexto);
 			}
 
 			if ((Gantt[t] != Gantt[t - 1]) && (Sobra[t - 1] > 1))
@@ -212,7 +212,9 @@ int main(void)
 			//printa no diagrama
 			//printf("T: %d  S: %c \n", t,Tarefa.id);
 
-			printf("\nGantt: %s\nP:  %d TC: %d \n\n", Gantt, Num_Premp, Troca_Contexto);
+			if (t == (time -1)){
+				printf("\nGantt: %s\nP:  %d+%d==%d TC: %d+%d==%d \n\n", Gantt, (Num_Premp-iddle_total),(iddle_total),(Num_Premp), (Troca_Contexto-iddle_total),(iddle_total),(Troca_Contexto));
+			}
 		}
 
 		return 0;
